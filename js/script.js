a3f7898839bae97c47b02a2a4de4b747000b7cdb
TheEnd <simplepage@bk.ru>
2022-12-05 19:27:19 +0700

    New commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Консольное приложение практика ч1 - Задание не выполнено самостоятельно :(
+
+// const number0Films = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//    count: number0Films,
+//    movies: {},
+//    actors: {},
+//    genres: [],
+//    privat: false
+// };
+
+// const a = prompt('Какой последний фильм вы смотрели?', ''),
+//       b = prompt('Какую оценку вы бы поставили фильму?', ''),
+//       c = prompt('Какой последний фильм вы смотрели?', ''),
+//       d = prompt('Какую оценку вы бы поставили фильму?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;       
+
+// console.log(personalMovieDB);
+
+
+console.log('hello');
+
+
+
