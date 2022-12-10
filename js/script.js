6428fc4d028ae8902231b6aff1cda277801c73d9
TheEnd <simplepage@bk.ru>
2022-12-10 16:25:30 +0700

    commit 2

--- js/script.js
+++ js/script.js
@@ -23,7 +23,51 @@
 // console.log(personalMovieDB);
 
 
-console.log('hello');
+// вывести значения с масива
+
+const userName = ['Алексей', 'Михаил', 'Аркадий', 'Евгений', 'Артём']; //lenght (длина масива 5, так как у нас в масиве 5 значений.)
+
+console.log(userName.length);
+
+let i = 0;
+
+while (i < userName.length) {
+   console.log(userName[i]);
+   i++;
+}
 
 
 
+// const usersName = {
+//    nameUser: {
+//       nameOne: 'Misha',
+//       nameTwo: 'Alex',
+//       nameThree: 'Sergei',
+//       nameFo: 'Max'
+//    }
+// };
+
+// let j = 0;
+
+// console.log(usersName.nameUser);
+
+// while ( j < nameUser.length) {
+//    console.log(usersName.nameUser);
+//    j++;
+// }
+
+
+/* Есть массив Друзей:
+
+var friends = [" Алексей", " Вячеслав", " Григорий", " Настя", " Николай", " Павел", " Любовь", " Егор", " Лэйла"];
+
+С помощью цикла while нужно вывести на экран всех друзей, начиная с Григория и заканчивая Любовью. При условии, что мы видим массив и знаем, как в нём расположены его элементы. */
+
+
+const friends = [" Алексей", " Вячеслав", " Григорий", " Настя", " Николай", " Павел", " Любовь", " Егор", " Лэйла"];
+let k = 2;
+
+while ( k < 7) {
+   console.log (friends[k]);
+   k++;
+}
